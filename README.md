c5cda8f7b2983a3cfaf44229d8a0b7a5796b52a0
Maryam Muska <maryammuska@ucla.edu>
2022-11-29 20:34:53 -0800

    changes to logic

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# Text-Editor
+# Text Editor
+        
+## Description
+        
+This application will allow the user to use the editor and be able to install it.
+
+## Table of Contents
+- [Installation](#installation)
+- [Heroku](#Heroku)
+- [Contribute](#Contribute)
+- [Questions](#Questions)
+        
+## Installation
+npm i
+npm start
+
+
+
+## Questions
+Reach me at: [Github](https://github.com/maryammmuska)
+
+Email me at: [Email](maryammuska@yahoo.com)
--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,27 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const useDb = await openDb('jate', 1);
+  const tx =useDb.transation('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({ value: content, id: 1, })
+  const result = await request;
+  console.log('putDb');
+}
+
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => 
+{
+  const useDb = await openDb('jate', 1);
+  const tx = useDb.transation('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.get(1);
+
+  const result = await request
+  console.log(result);
+}
+
 
 initdb();
